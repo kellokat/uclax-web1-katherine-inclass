1405a5b4c31ffee09cb0c0efe52a2aadeed793e2
Katherine Crowe <kellokat@gmail.com>
2021-02-02 00:04:16 -0500

    More tabbed navigation

---
+++ src/React/Pages/Welcome/Tabbed/TabbedNavItem.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import classnames from 'classnames';
+
+const TabbedNavItem = ({tabItem, changeTabs, chosenTab}) => {
+
+    const handleTabClick = () => {
+        changeTabs(tabItem);
+    }
+
+    const theClassName = classnames({
+        'TabbedNavItem': true,
+        'active': tabItem.title === chosenTab.title,
+    });
+
+    return (
+        <TabbedNavItemStyled 
+            className={ theClassName }
+            onClick={ handleTabClick }
+        >
+            { tabItem.title }
+        </TabbedNavItemStyled>
+    );
+}
+
+export default TabbedNavItem;
+
+const TabbedNavItemStyled = styled.div`
+        display: inline-block;
+        color: white;
+        line-height: 30px;
+        background-color: teal;
+        width: 100px;
+        text-align: center;
+        margin-right: 5px;
+        border-radius: 5px 5px 0px 0px;
+        font-size: 12px;
+        cursor: pointer;
+
+        background-color: #032d2d;
+
+        &:hover {
+            background-color: #032d2d;
+        }
+
+        &.active {
+            background-color: teal;
+        }
+    
+`;
